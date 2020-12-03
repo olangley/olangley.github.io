a1e24905abaec0fe4b280c7497641d3e50b304d5
Olivia S Langley <oliviaslangley@Olivias-MacBook-Pro.local>
2020-12-02 19:02:51 -0800

    refactored and moved javascript

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("thanks for stopping by!")
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $('body').addClass('loaded');
+  }, 1500);
+});
+
+$(window).scroll(function () {
+  var break1 = $('#home').height() - $('nav').height()/2;
+  var break2 = break1 + $('#about').height();
+  var break3 = break2 + $('#education').height();
+  if ($(this).scrollTop() > break3) {
+    $('nav').addClass('navbar2');
+    $('nav').removeClass('navbar3');
+  } else if ($(this).scrollTop() > break2) {
+    $('nav').addClass('navbar3');
+    $('nav').removeClass('navbar2');
+  } else if ($(this).scrollTop() > break1) {
+    $('nav').addClass('navbar2');
+    $('nav').removeClass('navbar3');
+  } else if ($(this).scrollTop() <= break1) {
+    $('nav').removeClass('navbar2');
+  }
+});
