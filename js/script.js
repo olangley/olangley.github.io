20c96e91775354e49aa75d13fabf2ef98d3a308e
Olivia S Langley <oliviaslangley@Olivias-MacBook-Pro.local>
2020-12-03 15:06:52 -0800

    restructured sticky navbar

--- js/script.js
+++ js/script.js
@@ -7,19 +7,20 @@ $(document).ready(function() {
 });
 
 $(window).scroll(function () {
-  var break1 = $('#home').height() - $('nav').height()/2;
+  var nav = $(".navbar");
+  var break1 = $('#home').height() - nav.height()/2;
   var break2 = break1 + $('#about').height();
   var break3 = break2 + $('#education').height();
   if ($(this).scrollTop() > break3) {
-    $('nav').addClass('navbar2');
-    $('nav').removeClass('navbar3');
+    nav.addClass('navbar2');
+    nav.removeClass('navbar3');
   } else if ($(this).scrollTop() > break2) {
-    $('nav').addClass('navbar3');
-    $('nav').removeClass('navbar2');
+    nav.addClass('navbar3');
+    nav.removeClass('navbar2');
   } else if ($(this).scrollTop() > break1) {
-    $('nav').addClass('navbar2');
-    $('nav').removeClass('navbar3');
+    nav.addClass('navbar2');
+    nav.removeClass('navbar3');
   } else if ($(this).scrollTop() <= break1) {
-    $('nav').removeClass('navbar2');
+    nav.removeClass('navbar2');
   }
 });
